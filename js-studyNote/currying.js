3d65ca9860156d8a39071902f585f94752dae7c3
YumeLoL <71587684+YumeLoL@users.noreply.github.com>
2022-10-27 19:09:06 +1100

    array

--- js-studyNote/currying.js
+++ js-studyNote/currying.js
@@ -8,17 +8,11 @@ const sum = (a) => {
 };
 // const multiply = (a) => (b) => a * b;
 
-console.log(sum(2)(3)(4));
+console.log(sum(2)(3)(8));
 
 // why we need this such strage notation - it is regarding to Currying.
 // create a currying function - Currying is a process in functional programming in which we can transform a function with multiple arguments into a sequence of nesting functions.
 // 函数柯里化：https://www.youtube.com/watch?v=vU3MVB5IxUY&t=4s
 // 通过函数调用继续返回函数的方式，实现多次接收参数最后统一处理的函数编码形式
-// React中的应用，例如
-{
-  /* <form onSubmit={onSubmit}>
-  <input onChange={saveFormData} type="text" name="username" />
-  <input onChange={saveFormData} type="password" name="password" />
-  <button>login</button>
-</form>; */
-}
+// React中的应用，例如 login 中 get input value.
+// https://github.com/YumeLoL/react-ts-tailwind-template/blob/main/src/pages/Login/index.tsx
