59290ddabdebc09e74e6a3155b058993a999fa8f
yume <yumeee.lol@gmail.com>
2022-11-15 12:13:51 +1100

    commit changes

---
+++ js-function/pureFunc.js
@@ -0,0 +1,5 @@
+// pure function： Given the same input, always return the same output
+// let output = console.log('welcome to') // console.log is always undefined
+// console.log('output is:', output)
+
+// https://segmentfault.com/a/1190000039807327
